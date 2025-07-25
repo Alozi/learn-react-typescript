cef753789e18673259678985839b6a3e65e68671
Alozi <alozimozi@gmail.com>
2025-07-25 17:42:06 +0300

    Adding a Data Model

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,3 +1,5 @@
+import Todo from "../models/todo";
+
 // const Todos: React.FC<{ items: string[] }> = (props) => {
 //   return (
 //     <ul>
@@ -9,11 +11,11 @@
 // };
 
 // export default Todos;
-export default function Todos({ items }: { items: string[] }) {
+export default function Todos({ items }: { items: Todo[] }) {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
