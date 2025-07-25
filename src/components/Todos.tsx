d0dcf6d7f3c1e85188e7e75099fc90d07f08f978
Alozi <alozimozi@gmail.com>
2025-07-25 17:56:09 +0300

    Create component TodoItem

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,7 @@
 import Todo from "../models/todo";
 
+import TodoItem from "./TodoItem";
+
 // const Todos: React.FC<{ items: string[] }> = (props) => {
 //   return (
 //     <ul>
@@ -15,7 +17,7 @@ export default function Todos({ items }: { items: Todo[] }) {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
