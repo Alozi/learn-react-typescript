2301e5d5ec56351a9b7803e3bc2819daa4ff6f22
Alozi <alozimozi@gmail.com>
2025-07-28 17:51:36 +0300

    Add useRef to the NewTodo component

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,13 +1,24 @@
+import { useRef } from "react";
+
 export default function NewTodo() {
+  const todoTextInput = useRef<HTMLInputElement>(null);
+
   function submitHandler(event: React.FormEvent) {
     event.preventDefault();
-    console.log(event);
+
+    const enteredText = todoTextInput.current!.value;
+    console.log(enteredText);
+
+    if (enteredText.trim().length === 0) {
+      //throw an error
+      return;
+    }
   }
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInput} />
       <button>Add Todo</button>
     </form>
   );
